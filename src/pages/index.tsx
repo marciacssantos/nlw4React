25e4a16867d7d513f0192460fa5e3ee628340fbd
Márcia Cristina da Silva Santos <marcia.cssantos95@gmail.com>
2021-03-08 12:40:48 -0300

    finalizando front do challengebox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import ExperienceBar from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 import Head from 'next/head'
 
@@ -21,7 +22,9 @@ export default function Home() {
         <CompletedChallenges />
         <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   )
